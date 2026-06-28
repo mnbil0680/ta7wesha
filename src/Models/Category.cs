b718e7e2c8d7b8cbe637fe7499d77fb795a1ca87
Mohamed Nabil <mnbil0680@gmail.com>
2026-06-28 08:08:05 +0300

    Add Category and its realtionship with User

--- src/Models/Category.cs
+++ src/Models/Category.cs
@@ -1,4 +1,14 @@
 public class Category : BaseEntity
 {
-    
+    public string Name { get; set; } = string.Empty;
+
+    public string? Icon { get; set; }
+
+
+    // Foreign Key
+    public int UserId { get; set; }
+
+    public User User { get; set; } = null!;
+
+    public ICollection<Expense> Expenses { get; set; } = new List<Expense>();
 }
--- src/Models/User.cs
+++ src/Models/User.cs
@@ -9,4 +9,7 @@ public class User : BaseEntity
     // Navigation Property
     public ICollection<Expense> Expenses { get; set; } = new List<Expense>();
     public ICollection<Income> Income { get; set; } = new List<Income>();
+    public ICollection<Category> Categories { get; set; } = new List<Category>();
+    public ICollection<Source> Sources { get; set; } = new List<Source>();
+
 }
